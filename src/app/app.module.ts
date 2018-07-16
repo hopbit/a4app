bf8503f5244785b334877973cc4417dc691891d6
Lukasz Siwinski <lsiwinski@gmail.com>
2018-07-16 22:56:10 +0200

    Generate Set component.
    Done with following command:
      ng g component components/set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SetComponent } from './components/set/set.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetComponent
   ],
   imports: [
     BrowserModule
