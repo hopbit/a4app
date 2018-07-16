54e81cfda97c2a670f6060a6ab9ebf0446f90164
Lukasz Siwinski <lsiwinski@gmail.com>
2018-07-16 23:33:16 +0200

    This won't 'compile'

--- src/app/components/set/set.component.ts
+++ src/app/components/set/set.component.ts
@@ -27,7 +27,7 @@ export class SetComponent implements OnInit {
     this.song = {
       artist: 'adam k, soha',
       title: 'long distance',
-      remix: 'original mix'
+      length: 'original mix'
     }
   }
 }
