02e82da94a7ce1fbc039b44daa0835b4347d9484
Lukasz Siwinski <lsiwinski@gmail.com>
2018-07-23 23:51:08 +0200

    Add button for removing tracks

--- src/app/components/set/set.component.ts
+++ src/app/components/set/set.component.ts
@@ -36,9 +36,9 @@ export class SetComponent implements OnInit {
     // this.year stays the same
     this.url = 'https://hopbit.github.io/DJ/progressive-awake/its-in-my-soul-2-july-2008.html';
     // this.tracks = []; // clear tracks
-    this.tracks.push({artist: 'mark knight, adam k, soha', title: 'from the speaker', remix: 'original mix', order: 1},);
-    this.tracks.push({artist: 'adam k, soha', title: 'long distance', remix: 'original mix', order: 2},);
-    this.tracks.push({artist: 'adam k, soha', title: 'twilight ', remix: 'original mix revisited', order: 3},);
+    this.tracks.push({artist: 'mark knight, adam k, soha', title: 'from the speaker', remix: 'original mix', order: 1});
+    this.tracks.push({artist: 'adam k, soha', title: 'long distance', remix: 'original mix', order: 2});
+    this.tracks.push({artist: 'adam k, soha', title: 'twilight ', remix: 'original mix revisited', order: 3});
   }
 
   tracksVisible() {
@@ -52,8 +52,17 @@ export class SetComponent implements OnInit {
 
   addNewTrack(newTrack) {
     console.log(`newTrack: ${newTrack}`);
-    this.tracks.unshift({artist: 'Unknown', title: newTrack, remix: null, order: this.tracks.length})
+    this.tracks.unshift({artist: 'Unknown', title: newTrack, remix: null, order: this.tracks.length});
     return false;
+  }
+
+  deleteTrack(trackObj) {
+    console.log(`trackObj: ${trackObj}`);
+    for (let i = 0; i < this.tracks.length; i++) {
+      if (this.tracks[i].order === trackObj.order) {
+        this.tracks.splice(i, 1);
+      }
+    }
   }
 }
 
