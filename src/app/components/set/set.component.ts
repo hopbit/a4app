8666f44bf20cfb36a443ad537d0e3b0c7af9a670
Lukasz Siwinski <lsiwinski@gmail.com>
2018-07-23 23:23:58 +0200

    Introduce a 'bug' in order to add more tracks on list when the button is clicked

--- src/app/components/set/set.component.ts
+++ src/app/components/set/set.component.ts
@@ -35,7 +35,7 @@ export class SetComponent implements OnInit {
     this.title = 'It\'s in my soule 2 (July 2008)';
     // this.year stays the same
     this.url = 'https://hopbit.github.io/DJ/progressive-awake/its-in-my-soul-2-july-2008.html';
-    this.tracks = []; // clear tracks
+    // this.tracks = []; // clear tracks
     this.tracks.push({artist: 'mark knight, adam k, soha', title: 'from the speaker', remix: 'original mix', order: 1},);
     this.tracks.push({artist: 'adam k, soha', title: 'long distance', remix: 'original mix', order: 2},);
     this.tracks.push({artist: 'adam k, soha', title: 'twilight ', remix: 'original mix revisited', order: 3},);
